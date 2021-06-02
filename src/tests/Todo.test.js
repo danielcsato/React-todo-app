2f9d45817bffd0482f03db0b6243545e90f43ef3
danielcsato <84443414+danielcsato@users.noreply.github.com>
2021-06-02 16:16:14 +0200

    upgraded enzyme adapter to v17

--- src/tests/Todo.test.js
+++ src/tests/Todo.test.js
@@ -1,8 +1,10 @@
 // setup file
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import 'jsdom-global/register';
+import Enzyme, { ShallowWrapper } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
+
 import React from 'react';
 import { shallow } from 'enzyme';
 
@@ -18,5 +20,13 @@ describe('Todo.jsx', () => {
     const todo = ['Task'];
     const wrapper = shallow(<Todo name={todo} />);
     expect(wrapper).toMatchSnapshot();
+  });
+  it('checks if delete button is clickable', () => {
+    const mockCLick = jest.fn();
+    const name = ['Task'];
+    const wrapper = shallow(<Todo name={name} />);
+    const button = wrapper.find('.icons');
+    button.props().onClick(mockCLick);
+    expect(mockCLick).toHaveBeenCalled();
   });
 });
